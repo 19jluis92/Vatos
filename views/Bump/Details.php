686df59ee27537b3e89b55127988e18c11e6a141
jose luis <jluis.ingcom@gmail.com>
2014-09-21 14:26:51 -0500

    bug for bump update

--- views/Bump/Details.php
+++ views/Bump/Details.php
@@ -2,7 +2,5 @@
 <h1>Details</h1>
 <?php 
 	echo "idBump: ", $id,"<br />";
-	echo "idPiece: ", $idPiece,"<br />";
-	echo "idSeverity: ", $idSeverity,"<br />";
-	echo "idInspection: ", $idInspection,"<br />";
+
  ?>
