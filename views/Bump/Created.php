5d46c18b64071f4b66fdff8fd926c824710584e5
jose luis <jluis.ingcom@gmail.com>
2014-09-21 14:38:03 -0500

    bugs in state and bump

--- views/Bump/Created.php
+++ views/Bump/Created.php
@@ -1,6 +1,6 @@
 <h1>CREATED</h1>
 <?php 
-	echo "idBump: ", $id,"<br />";
+	
 	echo "idPiece: ", $idPiece,"<br />";
 	echo "idSeverity: ", $idSeverity,"<br />";
 	echo "idInspection: ", $idInspection,"<br />";
--- views/State/Created.php
+++ views/State/Created.php
@@ -1,6 +1,6 @@
 <h1>CREATED</h1>
 <?php 
-	echo "idState: ", $id,"<br />";
+
 	echo "name: ", $name,"<br />";
-	echo "idCity: ", $idCity,"<br />";
+	echo "idState: ", $idState,"<br />";
  ?>
