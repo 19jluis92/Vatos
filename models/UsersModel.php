4a3d448b9633a0052e7a50a42b4df539c17f9746
jose luis <jluis.ingcom@gmail.com>
2014-10-05 10:52:24 -0500

    data base conecction  for user client and employee

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -4,15 +4,29 @@ class UsersModel extends Model{
 	private $email;
 	private $password;
 
-
+	function __construct(){
+		parent::__construct();
+		require("database_config.inc");
+		$this->db_driver = new mysqli($host,$user,$pass,$db);
+		if($this->db_driver->connect_error){
+			die('error de conexión a la base de datos');
+		}
+	}
 	/**
 	* @param string $email
 	* @param string $password
 	*/
 	function create($email, $password)
 	{
-		$this->email   = $email;
-		$this->password = $password;
+echo 'mama';
+		$this->email   = $this->db_driver->escape_string( $email);
+		$this->password = $this->db_driver->escape_string( $password);
+		$result = $this->db_driver->query("INSERT INTO user (email,password) values('$this->email','$this->password')");
+		if(!empty($this->db_driver->error)){
+			echo $this->db_driver->error;
+			return false;
+		}
+		// save element
 		return true;
 	}
 /**
@@ -21,8 +35,15 @@ class UsersModel extends Model{
 	* @param string $password
 	*/
 	function edit($email,$password){
-		$this->email   = $email;
-		$this->password = $password;
+
+		$this->email   = $this->db_driver->escape_string( $email);
+		$this->password = $this->db_driver->escape_string( $password);
+		$result = $this->db_driver->query("UPDATE  user SET email='$this->email',password='$this->password' WHERE idUser ='1'");
+		if(!empty($this->db_driver->error)){
+			echo $this->db_driver->error;
+			return false;
+		}
+		// save element
 		return true;
 	}
 /**
@@ -39,8 +60,16 @@ class UsersModel extends Model{
 		return true;
 	}
 
-	function details(){
-		return true;
+	function details($id){
+
+		$result = $this->db_driver->query("SELECT * FROM user where idUser='$id'");
+		
+		if(!empty($this->db_driver->error)){
+			echo $this->db_driver->error;
+			return false;
+		}
+		// save element
+		return $result;
 	}
 
 }
