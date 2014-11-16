da5f19d59b892b9aab311eebfd89fd16d6367f95
Erick Alfonso Fabian Garcia <fabianerickalfonso@gmail.com>
2014-11-16 12:56:42 -0600

    Fixes on users files

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -2,143 +2,138 @@
 require('controllers/Controller.php');
 require('mail.php');
 class UsersController extends Controller{
-	private $model;
-	/**
-	* Execute Actions based on the action selected from user in Query Args
-	*/
-	function __construct()
-	{
-		require('models/UsersModel.php');
-		$this->model = new UsersModel();
-	}
-	
-	function run()
-	{
-		$view = isset($_GET['view'])?$_GET['view']:'index';
-		switch($view)
-		{
-			case 'index':case 'list':
-			if($this->validateSession())			//Validate User and permissions
-			{$this->all();}
-			else{
-				echo'sin session';
-			}
+   private $model;
 
-			break;
-			case 'details':
-						//Validate User and permissions
-			$this->details();		
-			break;
-			case 'create':
-						//Validate User and permissions
-			$this->create();		
-			break;
-			case 'edit':
-						//Validate User and permissions
-			$this->edit();		
-			break;
-			case 'delete':
-						//Validate User and permissions
-			$this->delete();		
-			break;
-			default:
-			break;
-		}
-	}
-	private function create()
-	{
-			
-		//Validate Variables
-		$email   = $this->validateEmail($_POST['email']);
-		$password = $this->validateText($_POST['password']);
-		$result = $this->model->create($email, $password);
-		
-		//Insert Succesful
-		if($result)
-		{
-			//Load view
-			$message = 'Bienvenido Vato';
-			$mail = new Mail($email, $message);
-			$mail->send_mail();
-			require('views/User/Created.php');
-		}
-		else
-		{
-			require('views/Error.html');
-		}
-	}
-	/**
-	*@param $id
-	*@param return null
-	**/
-	private function delete(){
-		$id = $this->validateNumber($_POST['id']);
-		$result = $this->model->delete($id);
-		if($result)
-		{
-			require('views/User/Deleted.php');
-		}
-		else
-		{
-			require('views/Error.html');
-		}
-	}
-	/**
-	* Show details of car given it's Id
-	* @param id
-	* @return null, view rendered
-	*/
-	private function details(){
-		$id = $this->validateNumber($_POST['id']);
-		$result = $this->model->details($id);
-		
-		if(isset($result))
-		{
-			//Load view
-			require('views/User/Details.php');
-		}
-		else
-		{
-			//Ohh well... :(
-			require('views/Error.html');
-		}
-	}
-	/**
-	* Update user
-	* @param email
-	* @param password
-	* @return null, view rendered
-	*/
-	private function edit(){
-		$id = $this->validateText($_POST['id']);
-		$email = $this->validateText($_POST['email']);
-		$password = $this->validateText($_POST['password']);
-		$result = $this->model->edit($id,$email,$password);
-	}
-	/**
-	* Show all users in database
-	* @return null, view rendered
-	*/
-	private function all(){
-		$result = $this->model->index();
+   /**
+   * Execute Actions based on the action selected from user in Query Args
+   */
+   function __construct()
+   {
+      require('models/UsersModel.php');
+      $this->model = new UsersModel();
+   }
+   
+   function run()
+   {
+      $view = isset($_GET['view'])?$_GET['view']:'index';
+      switch($view)
+      {
+         case 'index':case 'list':
+               $this->validateSession() ? $this->all() : require('views/Error.html') ;
+         case 'details':
+               //Validate User and permissions
+               $this->details();    
+               break;
+         case 'create':
+               //Validate User and permissions
+               $this->create();     
+               break;
+         case 'edit':
+               //Validate User and permissions
+               $this->edit();    
+               break;
+         case 'delete':
+               //Validate User and permissions
+               $this->delete();     
+               break;
+         default:
+         break;
+      }
+   }
+   private function create()
+   {
+         
+      //Validate Variables
+      $email   = $this->validateEmail($_POST['email']);
+      $password = $this->validateText($_POST['password']);
+      $result = ($email && $password != 1) ? $this->model->create($email, $password) : 0 ;
+      //Insert Succesful
+      if($result)
+      {
+         //Load view
+         echo 'mail';
+         $message = 'Bienvenido Vato';
+         $mail = new Mail($email, $message);
+         $mail->send_mail();
+         require('views/User/Created.php');
+      }
+      else
+      {
+         require('views/Error.html');
+      }
+   }
+   /**
+   *@param $id
+   *@param return null
+   **/
+   private function delete(){
+      $id = $this->validateNumber($_POST['id']);
+      $result = $this->model->delete($id);
+      if($result)
+      {
+         require('views/User/Deleted.php');
+      }
+      else
+      {
+         require('views/Error.html');
+      }
+   }
+   /**
+   * Show details of car given it's Id
+   * @param id
+   * @return null, view rendered
+   */
+   private function details(){
+      $id = $this->validateNumber($_POST['id']);
+      $result = $this->model->details($id);
+      
+      if(isset($result))
+      {
+         //Load view
+         require('views/User/Details.php');
+      }
+      else
+      {
+         //Ohh well... :(
+         require('views/Error.html');
+      }
+   }
+   /**
+   * Update user
+   * @param email
+   * @param password
+   * @return null, view rendered
+   */
+   private function edit(){
+      $id = $this->validateText($_POST['id']);
+      $email = $this->validateText($_POST['email']);
+      $password = $this->validateText($_POST['password']);
+      $result = $this->model->edit($id,$email,$password);
+   }
+   /**
+   * Show all users in database
+   * @return null, view rendered
+   */
+   private function all(){
+      $result = $this->model->index();
 
-		if(isset($result))
-		{
-			//Load view
-			
-			require('views/User/Index.php');
-		}
-		else
-		{
-			//Ohh well... :(
-			require('views/Error.html');
-		}
-	}
+      if(isset($result))
+      {
+         //Load view
+         
+         require('views/User/Index.php');
+      }
+      else
+      {
+         //Ohh well... :(
+         require('views/Error.html');
+      }
+   }
 
-	public function authenticationForUser($name,$pass){
-		$result = $this->model->authentication($name,$pass);
-		return $result;
-	}
+   public function authenticationForUser($name,$pass){
+      $result = $this->model->authentication($name,$pass);
+      return $result;
+   }
 
 }
 ?>
